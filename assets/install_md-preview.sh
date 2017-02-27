915f253a97756abc786c5959eacb8b5b4bc415f8
JUDITH Florian <florian.judith@economat-armees.fr>
2017-02-27 01:29:47 +0100

    Bug fix

--- assets/install_md-preview.sh
+++ assets/install_md-preview.sh
@@ -4,5 +4,5 @@ pushd /alfresco/amps
 wget https://github.com/fjudith/md-preview/releases/download/1.2.1/parashift-mdpreview-repo-1.2.1.amp
 pushd /alfresco/amps_share
 wget https://github.com/fjudith/md-preview/releases/download/1.2.1/parashift-mdpreview-share-1.2.1.amp
-/alfresco/bin/apply_amps
+/alfresco/bin/apply_amps.sh
 popd
